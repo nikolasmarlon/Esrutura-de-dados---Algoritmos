5ec59f0d52a5cf5c818446953f19709ddcb23f77
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-03-05 19:14:51 -0300

    Acessando elementos do array através da iteração

--- array/array.js
+++ array/array.js
@@ -34,10 +34,36 @@ console.log('Tamanho do array meses ' + meses.length)
 
 //Posso inicializar o array já atribuindo valores para os índices 
 
-var mesesDoAno = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril')
+var mesesDoAno = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
 
 //imprime o tamanho do array
 console.log('Tamanho do array mesesDoAno ' + mesesDoAno.length) 
 
 
 //OBS. A melhor prática nao é utilizando Objeto Array()
+
+
+
+// ACESSANDO ELEMENTOS ATRAVÉS DA ITERAÇÃO
+
+console.log("Iterando arrays")
+
+//Imprimir todos os elementos do array
+for(var i = 0; i < mesesDoAno.length; i++){
+    console.log(`Mês: ${mesesDoAno[i]}`)
+}
+
+//Fibonacci   a soma dos dois anteriores é o resultado do posterior
+var fibonacci = []
+
+// iniciar com 3 posições
+fibonacci[0] = 0
+fibonacci[1] = 1
+fibonacci[2] = 1
+
+for(var i = 3; i < 30; i++){
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i -1]
+}
+
+
+console.log(fibonacci)
