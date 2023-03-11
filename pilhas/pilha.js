9d7aab6eb89dc03c1860d0242b1a2f1128e7ef2d
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-03-10 23:39:04 -0300

    Implementação dos métodos da classe pilha

--- pilhas/pilha.js
+++ pilhas/pilha.js
@@ -1,6 +1,34 @@
 
-
-
+//Para trabalhar com pilhas no JS usamos Classes, onde implementamos os metodos para manipularmos a pilha
+//Classe pilha
 function Pilha(){
     var itens = []
+
+    this.push = function(elemento){
+        //adiciona um novo item à pilha
+    }
+
+    this.pop = function(){
+        //remove o item do topo da pilha
+    }
+
+    this.peek = function(){
+        //devolve o elemento que está no topo da pilha
+    }
+
+    this.isEmpty = function(){
+        //Informa se a pilha está vazia ou não
+    }
+
+    this.clear = function(){
+        //Limpa a pilha
+    }
+
+    this.size = function(){
+        //informar o tamanho da pilha
+    }
+
+    this.print = function(){
+        //imprime a pilha no console
+    }
 }
