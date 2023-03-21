1f8077ffe17ea96ff5f9e327d6b4aa6aa35d68ba
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-03-20 22:18:18 -0300

    implementando os métodos da classe fila

--- filas/script.js
+++ filas/script.js
@@ -1,27 +1,38 @@
 
 
 //clase Fila
-
 function Fila() {
     var itens = []
 
-    this.enfileirar = function(){
+    this.enfileirar = function(elemento){
         //adiciona um novo elemento
+        itens.push(elemento)
+
     }
 
     this.desenfileirar = function(){
-        //remove elemento 
+        //remove elemento
+        return itens.shift() 
     }
 
     this.frente = function(){
         //retorna o primeiro elemento da fila
+        return itens[0]
+    }
+    
+    this.verSeEstaVazio = function(){
+        //verifica se a fila está vazia ou não
+        return itens.length === 0 
     }
 
     this.tamanho = function(){
         //retorna o tamanho da fila
+        return itens.length
+
     }
 
     this.imprimir = function(){
         //imprime a fila no console
+        console.log(itens.toString())
     }
 }
