22257aa3454e81aee2c58ec4da19d6653c08fd5e
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-03-20 22:12:38 -0300

    iniciando a classe fila

---
+++ filas/script.js
@@ -0,0 +1,27 @@
+
+
+//clase Fila
+
+function Fila() {
+    var itens = []
+
+    this.enfileirar = function(){
+        //adiciona um novo elemento
+    }
+
+    this.desenfileirar = function(){
+        //remove elemento 
+    }
+
+    this.frente = function(){
+        //retorna o primeiro elemento da fila
+    }
+
+    this.tamanho = function(){
+        //retorna o tamanho da fila
+    }
+
+    this.imprimir = function(){
+        //imprime a fila no console
+    }
+}
